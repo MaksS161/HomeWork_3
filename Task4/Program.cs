27e3ad1b1d7fc360f22533ff7d37c01ad7447d8c
MaksS161 <https://github.com/MaksS161>
2023-08-22 16:43:59 +0300

    Encoding

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,34 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задание 4
+Создайте класс «Веб-сайт». Необходимо хранить в
+полях класса: название сайта, путь к сайту, описание
+сайта, ip адрес сайта. Реализуйте методы класса для ввода
+данных, вывода данных, реализуйте доступ к отдельным
+полям через методы класса.
+*/
 
-
-Console.WriteLine("Введите массив данных!");
-var arr = new char[9];
-var brr = new char[4] { '6', '8', '8', '7' };
-
-
-for (int i = 0; i < arr.Length; i++)
-{
-    arr[i] = Convert.ToChar(Console.ReadLine());
-}
-
-void filterData(char[] arr, char[] brr)
-{
-    var crr = new char[9];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        int count = 0;
-        for (int j = 0; j < brr.Length; j++)
-        {
-            if (arr[i] == brr[j]) count++;
-        }
-        if (count == 0) crr[i] = arr[i];
-    }
-
-    for (int i = 0; i < crr.Length; i++)
-    {
-        Console.Write(crr[i]);
-    }
-}
-
-filterData(arr, brr);
