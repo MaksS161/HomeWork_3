ca177db682d87de3f2c1f507b4bbac93e2060e0c
MaksS161 <https://github.com/MaksS161>
2023-08-22 07:33:40 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задание 1
+Напишите метод, который отображает квадрат из
+некоторого символа. Метод принимает в качестве параметра: длину стороны квадрата, символ.
+*/
+
+
+Console.WriteLine("Введите длину стороны квадрата: ");
+var length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите символ:");
+char symbol = Convert.ToChar(Console.ReadLine());
+
+print(length, symbol);
+void print(int length, char symbol)
+{
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length; j++)
+        {
+            Console.Write($"{symbol} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задание 2
+Напишите метод, который проверяет является ли
+переданное число «палиндромом». Число передаётся в
+качестве параметра. Если число палиндром нужно вернуть из метода true, иначе false.
+Палиндром — число, которое читается одинаково как
+справа налево, так и слева направо. Например:
+1221 — палиндром;
+3443 — палиндром;
+7854 — не палиндром.
+*/
+
+
+using System.Runtime.InteropServices;
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\nПроверка:");
+bool checkPalindrom (int number)
+{
+    int revers = 0;
+    int buffer = 0;
+    int initial = number;
+    for (int i = 1; number != 0;i++)
+    {
+        buffer = number % 10;
+        number = number / 10;
+        revers = buffer + (revers * 10);
+    }
+    //Console.WriteLine(revers);
+
+    if (revers == initial) return true;
+    else return false;
+}
+
+Console.WriteLine(checkPalindrom(number));
+
+
+
+
